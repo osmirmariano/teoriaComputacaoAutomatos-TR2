b2260ab9f89375918e836d32afc3f7457329f1cf
osmirmariano <osmirmariano@uft.edu.br>
2016-06-03 02:33:50 -0300

    Atualizações sem sucesso

--- Expressao.cpp
+++ Expressao.cpp
@@ -11,6 +11,7 @@ class Expressao{
 		int cont;
 		vector<string> alfabetos;
         vector<char> alfa;
+        //vector<char> palavra;
 
     public:
 		Expressao(){
@@ -19,12 +20,48 @@ class Expressao{
 
 		~Expressao();
 
-		/*--------------------------- AUTOMATO FINITO DETERMINÍSTICO DA LINGUAGEM ------------------------------*/
+		/*--------------------------- AUTOMATO FINITO DETERMINÍSTICO DA LINGUAGEM -------------------------*/
 		void AFD(){
-
+            cout << " FUNÇÃO TRNASIÇÃO  |     a     |     b     " << endl;
+            cout << "---------------------------------------------" << endl;
+            cout << "      ->qo         |     q1    |     qo     " << endl;
+            cout << "---------------------------------------------" << endl;
+            cout << "        q1         |     q2    |     q3     " << endl;
+            cout << "---------------------------------------------" << endl;
+            cout << "        q2         |     q4    |     q5     " << endl;
+            cout << "---------------------------------------------" << endl;
+            cout << "        q3         |     q6    |     q7     " << endl;
+            cout << "---------------------------------------------" << endl;
+            cout << "       *q4         |     q4    |     q5     " << endl;
+            cout << "---------------------------------------------" << endl;
+            cout << "       *q5         |     q6    |     q7     " << endl;
+            cout << "---------------------------------------------" << endl;
+            cout << "       *q6         |     q2    |     q3     " << endl;
+            cout << "---------------------------------------------" << endl;
+            cout << "       *q7         |     q1    |     qo     " << endl;
+            cout << "---------------------------------------------" << endl << endl;
 		};
 
-		/*-----------------------------FUNÇÃO DE TRNASIÇÃO ESTENDIDA DA LINGUAGEM ------------------------------*/
+        void AFD(string palavra){
+            
+        };
+
+        /*-----------------------------FUNÇÃO QUE VERIFICA SE PERTENCE AO AFD------------------------------*/
+        void verifPertenceAFD (string palavra){
+            int tamanho = 0;
+            tamanho = palavra.length();
+            /*for(){
+                if(palac)
+            }
+
+            if(palavra[tamanho-1] == q4 || palavra[tamanho-1] == q5 || palavra[tamanho-1] == q6 || palavra[tamanho-1] == q7)
+                cout << " PALAVRA ACEITA PELO AFD" << endl;
+            else
+                cout << " PALAVRA NÃO ACEITA PELO AFD" << endl;*/
+            
+        }
+
+		/*-----------------------------FUNÇÃO DE TRNASIÇÃO ESTENDIDA DA LINGUAGEM --------------------------*/
 		void funcaoTransicaoEstendida(string palavra){ // δ(q,w) = δ(δ(q,x),a)
             int tam = 0, y=0;
             string w;
@@ -37,6 +74,29 @@ class Expressao{
                 cout << " Subpalavra: " << w << endl;
                 //w="";
             }
-
         };
+
+        void naoSeiAinda(){
+            int quantidade;
+            string estados, armazena, alfabeto;
+
+            cout << " INFORME O ALFABETO: ";
+            cin >> alfabeto;
+            cout  << endl;
+            cout << " INFORME A QUANTIDADE DE ESTADOS: ";
+            cin >> quantidade;
+            for (int x = 0; x < quantidade; x++){
+                cout << " " << x+1 << "º ESTADO: ";
+                cin >> estados;
+                armazena += estados;                    
+            }
+            cout << endl << " ESTADOS: " << armazena << endl;
+            cout << " ALFABETO: " << alfabeto << endl;
+            int valor = armazena.length();
+            cout << " TAMANHO: " << valor;
+            for (int x = 0; x < valor-1; x++){
+
+            }
+        }
+        
 };
--- main.cpp
+++ main.cpp
@@ -5,42 +5,46 @@
 using namespace std;
 
 int main(){
-    int op;
-    string expressao = "(a+b)*a(a+b)(a+b)", palavra;
+    int op, quantidade;
+    string palavra;
 
-    cout << endl << "----------------- V. 1.0.0 ------------------  " << endl;
     Expressao *exp = new Expressao();
+   
     do{
         cout << endl << "---------------------------------------------" << endl;
         cout << "\t MENU PRINCIPAL" << endl;
         cout << "---------------------------------------------" << endl;
-        cout << " 1 -- LINGUAGEM REGULAR" << endl;
+        cout << " EXPRESSÃO REGULAR DEFINIDA: (a+b)*a(a+b)(a+b)" << endl;
+        cout << "---------------------------------------------" << endl;
+        cout << " 1 -- INSERÇÃO DE PALAVRA" << endl;
         cout << " 2 -- AFD DA LINGUAGEM" << endl;
-        cout << " 3 -- FUNCAO DE TRANSICAO ESTENDIDA POR INDUCAO" << endl;
+        cout << " 3 -- FUNCAO DE TRANSICAO ESTENDIDA" << endl;
+        cout << " 4 -- INSERÇÃO DOS ESTADOS" << endl;
         cout << " 0 -- SAIR " << endl;
         cout << "---------------------------------------------" << endl;
         cout << "OPCAO: ";
         cin >> op;
         cout << "---------------------------------------------" << endl << endl;
-        system("cls");
 
         switch(op){
             case 1:{
                 cout << endl << "---------------------------------------------" << endl;
-                cout << "\t LINGUAGEM REGULAR" << endl;
+                cout << "\t INSERÇÃO DE PALAVRAS" << endl;
                 cout << "---------------------------------------------" << endl;
-                cout << " " << expressao << endl;
+                cout << " PALAVRA: ";
+                cin >> palavra;
+                exp->verifPertenceAFD(palavra);
                 break;
             }
             case 2:
                 cout << endl << "---------------------------------------------" << endl;
                 cout << "\t AFD DA LINGUAGEM " << endl;
                 cout << "---------------------------------------------" << endl;
-                cout << " Projetar AFD no programa ou em PDF." << endl;
+                exp->AFD();
                 break;
             case 3:
                 cout << endl << "---------------------------------------------" << endl;
-                cout << "\t FUNCAO DE TRANSICAO ESTENDIDA POR INDUCAO" << endl;
+                cout << "\t FUNCAO DE TRANSICAO ESTENDIDA" << endl;
                 cout << "---------------------------------------------" << endl;
                 cout << " Mostrar  funcao de transicao estendida por inducao sobre o comprimento da palavra de entrada, ateh chegar a base" << endl;
                 cout << " Palavra: ";
@@ -49,6 +53,13 @@ int main(){
                     //verificar se último elemento da palavra fica no estado final
                         // se palavra for aceita retornar estado final
                 exp->funcaoTransicaoEstendida(palavra);
+                break;
+            case 4:
+                cout << endl << "---------------------------------------------" << endl;
+                cout << "\t INSERÇÃO DOS ESTADOS" << endl;
+                cout << "---------------------------------------------" << endl;
+                exp->naoSeiAinda();
+                //cout << "ESTADOS: " << armazena << endl;
                 break;
             case 0:
                 cout << "PROGRAMA ENCERRADO COM SUCESSO!" << endl;
